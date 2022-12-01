67df1346d48c06243987f5b2644f33004ef5fbe8
Mihaela1117 <107248062+Mihaela1117@users.noreply.github.com>
2022-12-01 22:00:07 +0200

    Add files via upload

---
+++ MK/MK/Animal.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MK
+{
+    public class Animal
+    {
+        public int Id { get; set; }
+        public int AnimalTypeId { get; set; }
+        public AnimalType AnimalTypes { get; set; }
+    }
+}
---
+++ MK/MK/Context.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MK
+{
+    public class Context:DbContext
+    {
+        public DbSet<Animal> Animals { get; set; }
+        public DbSet<AnimalType> Types { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder builder) =>
+            builder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=Animal;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+    }
+}
